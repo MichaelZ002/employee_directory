9aaa677e1e310420c18cfc04d51eb8af982bc6be
Michael Zaslavsky <mikezasl@gmail.com>
2020-09-22 22:23:28 -0500

    Updates

---
+++ precache-manifest.152aad48f59d9f4d2bd8019fd97e0dae.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d013d2a0c1492acb13fa272ede83520",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91a578d689d8b6fdf7bf",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "964d0d5cb2d73cdd2513",
+    "url": "/static/css/main.f4ffa218.chunk.css"
+  },
+  {
+    "revision": "91a578d689d8b6fdf7bf",
+    "url": "/static/js/2.c4c94f30.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c4c94f30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "964d0d5cb2d73cdd2513",
+    "url": "/static/js/main.dc6eb27d.chunk.js"
+  },
+  {
+    "revision": "ed0fae93a1705cfe61bf",
+    "url": "/static/js/runtime-main.61c3aabd.js"
+  },
+  {
+    "revision": "4add802c839b6ae394aee4f97e4d1b85",
+    "url": "/static/media/coolDude.4add802c.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);

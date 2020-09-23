42fac2902ecc0f6ae6e2e0598bfa28f5f3a464f3
Michael Zaslavsky <mikezasl@gmail.com>
2020-09-22 21:43:53 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,104 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Comp from './components/Comp'
+
+const employees = [
+  {
+    id: 1,
+    firstName: "Cool",
+    lastName: "Dude",
+    email: "cooldude1@coolguy.com",
+    favoriteFood: "Pizza",
+  },
+  {
+    id: 2,
+    firstName: "John",
+    lastName: "Smith",
+    email: "JohnSmith1@coolguy.com",
+    favoriteFood: "Burger",
+  },
+  {
+    id: 3,
+    firstName: "Left",
+    lastName: "Right",
+    email: "LeftRight1@coolguy.com",
+    favoriteFood: "Pizza",
+  },
+  {
+    id: 4,
+    firstName: "Top",
+    lastName: "Bottom",
+    email: "TopBottom1@coolguy.com",
+    favoriteFood: "Sour Patch Kids",
+  },
+  {
+    id: 5,
+    firstName: "Adam",
+    lastName: "Allen",
+    email: "cooldude1@coolguy.com",
+    favoriteFood: "Sour Patch Kids",
+  },
+  {
+    id: 6,
+    firstName: "Ben",
+    lastName: "D",
+    email: "BenD1@coolguy.com",
+    favoriteFood: "Burger",
+  },
+  {
+    id: 7,
+    firstName: "Michael",
+    lastName: "Zaslavsky",
+    email: "MichaelZaslavsky@coolguy.com",
+    favoriteFood: "Burger",
+  },
+  {
+    id: 8,
+    firstName: "Coal",
+    lastName: "DR",
+    email: "BenD1@coolguy.com",
+    favoriteFood: "Burger",
+  },
+  {
+    id: 9,
+    firstName: "R",
+    lastName: "Coal",
+    email: "BenD1@coolguy.com",
+    favoriteFood: "Burger",
+  },
+  {
+    id: 0,
+    firstName: "Some",
+    lastName: "Dude",
+    email: "BenD1@coolguy.com",
+    favoriteFood: "Burger",
+  },
+];
 
 function App() {
+
+  const [search, setSearch] = useState("");
+  const [sort, setSort] = useState("asc");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className="title text-center">Cool Guy Employee Directory</h1>
+      <form>
+        <div className="search form-group form-inline d-flex justify-content-center">
+          <label htmlFor="search">Search Name:</label>
+          <input id="search" onChange={ event => setSearch(event.target.value) } placeholder="Michael Zaslavsky " />
+        </div>
+        <div className="sort form-group form-inline d-flex justify-content-center">
+          <label>Sort Name:</label>
+          <label className="radio-inline"><input type="radio" name="sort" value="asc" checked={ sort === "asc" } onChange={ event => setSort(event.target.value) } />Ascending</label>
+          <label className="radio-inline"><input type="radio" name="sort" value="desc" checked={ sort === "desc" } onChange={ event => setSort(event.target.value) } />Descending</label>
+        </div>
+      </form>
+      <Comp search={search} sort={sort} employees={employees}/>
+    </>
   );
 }
+
 
 export default App;
---
+++ src/components/Comp.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+function Comp(props) {
+    const searchedSorted = props.employees.filter( employee => (employee.firstName + " " + employee.lastName).toLowerCase().includes(props.search.toLowerCase())).sort( (a,b) => {
+        let x = (a.firstName + a.lastName).toLowerCase();
+        let y = (b.firstName + b.lastName).toLowerCase();
+        if (props.sort === "asc") {
+            if (x < y) return -1;
+            if (x > y) return 1;
+        } else {
+            if (x > y) return -1;
+            if (x < y) return 1;
+        }
+        return 0;
+    });
+
+    return (
+        <table className="table table-striped table-hover">
+            <thead className="thead-dark">
+                <tr>
+                    <th scope="col">Name:</th>
+                    <th scope="col">Phone:</th>
+                    <th scope="col">Email:</th>
+                    <th scope="col">Favorite Food:</th>
+                </tr>
+            </thead>
+            <tbody>
+                {searchedSorted.map(employee => (
+                    <tr key={employee.id}>
+                        <th scope="row">{employee.firstName + " " + employee.lastName}</th>
+                        <td>{employee.phone}</td>
+                        <td>{employee.email}</td>
+                        <td>{employee.favoriteFood}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    );
+}
+
+export default Comp;
